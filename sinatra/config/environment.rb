e0d97d54de7705b00d0a81081c0d4cb4fefc8ca7
k-koech <koechkelvin97@gmail.com>
2023-06-08 08:11:04 +0300

    deployment

--- sinatra/config/environment.rb
+++ sinatra/config/environment.rb
@@ -6,7 +6,13 @@ ENV['RACK_ENV'] ||= "development"
 require 'bundler/setup'
 Bundler.require(:default, ENV['RACK_ENV'])
 
-
+# Database configrurations
+configure do
+    set :database, {
+      adapter: 'sqlite3',
+      database: 'db/development.sqlite3'
+    }
+  end
   
 # Require in all files in 'app' directory
 require_all 'app'
